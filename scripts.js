f53c97366c9593e3eada7033e5702e80659eb3cf
Rouzbeh Hatamy <roozbeh.hatamy@gmail.com>
2020-06-24 00:59:15 +0430

    finish layout

--- scripts.js
+++ scripts.js
@@ -12,3 +12,20 @@ $('#all').on('click', function() {
   $('input:not(#all)').prop('checked', $(this).is(':checked'));
   billGenarator();
 });
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    const scroll = $(window).scrollTop();
+    if (scroll >= 50) {
+      $('.header').css('border-radius', '0px');
+    } else {
+      $('.header').css('border-top-left-radius', '36px');
+      $('.header').css('border-top-right-radius', '36px');
+    }
+  });
+  //
+  $('.fa-plus').on('click', function() {
+    console.log($(this).attr('value'));
+    console.log($(this).attr('data-counter'));
+  });
+});
